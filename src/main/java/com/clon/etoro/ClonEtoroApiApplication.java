5793760ec772cd51b67f519bf68d075238d0b7cc
Cristian Contreras <cristian.contreras9712@gmail.com>
2025-12-09 10:34:48 -0500

    First commit

---
+++ src/main/java/com/clon/etoro/ClonEtoroApiApplication.java
@@ -0,0 +1,13 @@
+package com.clon.etoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonEtoroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonEtoroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clon/etoro/ClonEtoroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clon.etoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonEtoroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
